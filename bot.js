47abae646e129c31ddf4d6fafbcf87833bb7bd25
lekejosh <lekejosh6wf@gmail.com>
2022-08-29 23:06:28 +0100

    inline_keyboard

--- bot.js
+++ bot.js
@@ -3,15 +3,15 @@ const bot = new Telegraf("5605755705:AAFcqIVnFlN3r9LAxbWumf7_w05g7Cdapj8");
 const axios = require("axios");
 
 bot.command("start", (ctx) => {
-  let startMessage = "Welcome, This Bot is a Buy ";
+  let startMessage =
+    "Hello!\n 1) Please click the link below(Admins only). \n 2) Click the 'Start' Button at the bottom of the screen. \n 3) if you want to add a token to the group, choose 'Add Token', or; \n 4) if you want to changethe settings for an already added token, choose 'Change Token Settings'. \n \n You will need to click this link everytime you want to enter the menus~";
   bot.telegram.sendMessage(ctx.chat.id, startMessage, {
     reply_markup: {
-      inline_keybord: [
-        [{ text: "Test Message", callback_data: "Price" }],
+      inline_keyboard: [
         [
           {
-            text: "CoinMarketCap",
-            url: "https://coinmarketcap.com",
+            text: "Click me",
+            url: "https://t.me/BobbyBuyBot?start=-744283958",
           },
         ],
       ],
