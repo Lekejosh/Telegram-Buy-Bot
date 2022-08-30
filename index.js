82dbc6a85ce2d9ba5cff821ba85e5b8baf4da614
lekejosh <lekejosh6wf@gmail.com>
2022-08-31 00:05:38 +0100

    update

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const text = require("./text.json");
 
 const bot = new Telegraf("5605755705:AAFcqIVnFlN3r9LAxbWumf7_w05g7Cdapj8");
 
+const model = new mongoose.Schema({
+  name: String,
+  userName: String,
+  userId: Number,
+  chatId: Number,
+  firstName: String,
+  status: String,
+});
+
 bot.use(function (ctx, next) {
   /// or other chat types...
   // if( ctx.chat.type !== 'channel' ) return next();
@@ -29,14 +38,21 @@ bot.use(function (ctx, next) {
 });
 
 bot.start((ctx) => {
-  if (ctx.from._is_in_admin_list) {
+  //   new model({
+  //     name: ctx.from.first_name,
+  //     userId: ctx.from.id,
+  //     userName: ctx.from.username,
+  //     chatId: ctx.chat.id,
+  //     status: ctx.status,
+  //   }).save();
+  if (ctx.from.id && ctx.from._is_in_admin_list) {
     bot.telegram.sendMessage(ctx.chat.id, text.welcome, {
       reply_markup: {
         inline_keyboard: [
           [
             {
-              text: "Click me",
-              url: `t.me/leke_tut_buyBot?start=${ctx.chat.id}`,
+              text: "Proceed",
+              callback_data: "add",
             },
           ],
         ],
@@ -48,6 +64,101 @@ bot.start((ctx) => {
     });
   }
 });
+bot.action("setting", (ctx) => {
+  if (ctx.from._is_in_admin_list) {
+    ctx.deleteMessage();
+    bot.telegram.sendMessage(ctx.chat.id, text.setting, {
+      reply_markup: {
+        inline_keyboard: [
+          [
+            {
+              text: "Reset Token",
+              url: "https://t.me/leke_tut_buyBot",
+            },
+          ],
+          [{ text: "Back", callback_data: "start" }],
+        ],
+      },
+    });
+  } else {
+    return;
+  }
+});
+
+bot.action("add", (ctx) => {
+  if (ctx.from._is_in_admin_list) {
+    ctx.deleteMessage();
+    bot.telegram.sendMessage(ctx.chat.id, text.setting, {
+      reply_markup: {
+        inline_keyboard: [
+          [
+            {
+              text: "Add New Token",
+              callback_data: "set",
+            },
+          ],
+          [{ text: "Change token setting", callback_data: "setting" }],
+        ],
+      },
+    });
+  } else {
+    return;
+  }
+});
+
+bot.action("set", (ctx) => {
+  if (ctx.from._is_in_admin_list) {
+    ctx.deleteMessage();
+    bot.telegram.sendMessage(
+      ctx.chat.id,
+      text.set + " " + `${ctx.chat.title}`,
+      {
+        reply_markup: {
+          inline_keyboard: [
+            [
+              {
+                text: "ETH",
+                callback_data: "eth",
+              },
+            ],
+            [{ text: "BSC", callback_data: "bsc" }],
+          ],
+        },
+      }
+    );
+  } else {
+    return;
+  }
+});
+
+bot.action("eth", (ctx) => {
+  if (ctx.from._is_in_admin_list) {
+    ctx.deleteMessage();
+    bot.telegram.sendMessage(ctx.chat.id, text.token, {
+      reply_markup: {
+        inline_keyboard: [
+          [
+            {
+              text: "Uniswap",
+              callback_data: "uniswap",
+            },
+          ],
+        ],
+      },
+    });
+  } else {
+    return;
+  }
+});
+
+// const redirect = (ctx) => {
+//   if (ctx.chat.id && ctx.from._is_in_admin_list) {
+//     bot.command(`start=${ctx.chat.id}`);
+//     return ctx.reply(text.start);
+//   } else {
+//   }
+// };
+// redirect();
 
 const init = async () => {
   mongoose.connect("mongodb://localhost:27017/telegram", {
